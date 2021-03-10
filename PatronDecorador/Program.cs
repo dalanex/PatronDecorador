1d973f1ac7a5dc79564e372ea601500485ff14f6
David Landete Exposito <dalanex@upv.edu.es>
2021-03-10 13:44:09 +0100

    inicio del proyecto

--- PatronDecorador/Program.cs
+++ PatronDecorador/Program.cs
@@ -2,11 +2,32 @@
 
 namespace PatronDecorador
 {
-    class Program
+    class Cliente
     {
+        private static string direccionEnvio, direccionOrigen;
+        private static string codigoPostalDestino, codigoPostalOrigen;
+        private static double peso;
+        private Envio envio;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("WELCOME TO AMAZING!!");
+            Console.WriteLine("---------------------");
+        }
+
+        private void obtenerDetalles()
+        {
+            Console.WriteLine("First of all, introduce all data:");
+            Console.WriteLine("-- Destination address");
+            direccionEnvio = Console.ReadLine();
+            Console.WriteLine("-- Origen address");
+            direccionOrigen = Console.ReadLine();
+            Console.WriteLine("-- Destination CP");
+            codigoPostalDestino = Console.ReadLine();
+            Console.WriteLine("-- Origen CP");
+            codigoPostalOrigen = Console.ReadLine();
+            Console.WriteLine("-- Weight");
+            peso = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
