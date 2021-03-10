33b4cdc401a0ccb43132bf8e62c182fe9b0723a0
David Landete Exposito <dalanex@upv.edu.es>
2021-03-10 14:10:33 +0100

    implementación básica

---
+++ PatronDecorador/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PatronDecorador
+{
+    class Cliente
+    {
+        private string direccionDestino, direccionOrigen;
+        private string cpDestino, cpOrigen;
+        private double peso;
+        private Envio envio;
+        private static Cliente cliente;
+
+        public Cliente() { }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO AMAZING!!");
+            Console.WriteLine("---------------------");
+            cliente = new Cliente();
+            cliente.enviar();
+        }
+
+        public void enviar()
+        {
+            obtenerDetalles();
+            string detalles;
+            envio = new Envio(direccionDestino, direccionOrigen, cpDestino, cpOrigen, peso);
+            detalles = envio.enviar();
+            Console.WriteLine(detalles);
+            Console.WriteLine("\n-- Final price: ");
+            Console.Write(envio.conseguirCoste() + "\n");
+        }
+
+        private void obtenerDetalles()
+        {
+            Console.WriteLine("\nFirst of all, introduce all data:");
+            Console.WriteLine("-- Destination address");
+            direccionDestino = Console.ReadLine();
+            Console.WriteLine("-- Origen address");
+            direccionOrigen = Console.ReadLine();
+            Console.WriteLine("-- Destination CP");
+            cpDestino = Console.ReadLine();
+            Console.WriteLine("-- Origen CP");
+            cpOrigen = Console.ReadLine();
+            Console.WriteLine("-- Weight");
+            peso = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
--- PatronDecorador/Envio.cs
+++ PatronDecorador/Envio.cs
@@ -6,5 +6,33 @@ namespace PatronDecorador
 {
     class Envio
     {
+        private int envioId;
+        private string direccionDestino, direccionOrigen;
+        private string cpDestino, cpOrigen;
+        private double peso;
+
+        public Envio(string direccionDestino, string direccionOrigen, string cpDestino, string cpOrigen, double peso)
+        {
+            this.direccionDestino = direccionDestino;
+            this.direccionOrigen = direccionOrigen;
+            this.cpDestino = cpDestino;
+            this.cpOrigen = cpOrigen;
+            this.peso = peso;
+            envioId = conseguirId();
+        }
+
+        public string enviar()
+        {
+            string res = "\nThe shipment with ID: " + envioId;
+            res += "\nWill be delivered in: " + direccionDestino + ", " + cpDestino;
+            res += "\nSent from: " + direccionOrigen + ", " + cpOrigen;
+            return res;
+        }
+
+        public double conseguirCoste() { return peso*0.39; }
+
+        private static int lastEnvioId = 0;
+
+        private int conseguirId() { return ++lastEnvioId; }
     }
 }
