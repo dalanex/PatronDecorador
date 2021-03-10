ba33694e1bdac3a5c26e2b7017adb447c58004da
David Landete Exposito <dalanex@upv.edu.es>
2021-03-10 13:29:55 +0100

    Agregar archivos de proyecto.

---
+++ PatronDecorador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatronDecorador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
