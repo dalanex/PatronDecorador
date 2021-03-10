5bedc8de29f9d85c7b1a4d0a61e2f912c080d499
David Landete Exposito <dalanex@upv.edu.es>
2021-03-10 14:13:31 +0100

    creacion solucion añadiendo tipos de envio

---
+++ Decorador añadiendo tipos/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Decorador_añadiendo_tipos
+{
+    class Cliente
+    {
+        private string direccionDestino, direccionOrigen;
+        private string cpDestino, cpOrigen;
+        private double peso;
+        private Envio envio;
+        private static Cliente cliente;
+
+        public Cliente() { }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO AMAZING!!");
+            Console.WriteLine("---------------------");
+            cliente = new Cliente();
+            cliente.enviar();
+        }
+
+        private void enviar() { }
+    }
+}
---
+++ Decorador añadiendo tipos/Envio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorador_añadiendo_tipos
+{
+    class Envio
+    {
+    }
+}
